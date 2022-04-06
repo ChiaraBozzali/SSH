cb54783cae63c418e840574b1927f3355a9336d5
Chiara <chiara.bozzali@fermi.mo.it>
2022-04-06 08:44:54 +0200

    My SSH first commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings 
+from django.db import models 
+from django.utils import timezone 
+
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    data_ora = models.DateTimeField(default=None)
+    temp = models.IntegerField(default=None)
+    hum = models.IntegerField(default=None)
+    co2 = models.IntegerField(default=None)
+    voc = models.IntegerField(default=None)
+    pmio = models.IntegerField(default=None)
+    pm25 = models.IntegerField(default=None)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.data_ora)
